0a714caf8dea0d9723b65377a9eef88276a67b7c
Ieuan Harries <2549037h@student.gla.ac.uk>
2022-01-18 18:14:59 +0000

    Completed up to end of Ch. 3.5

---
+++ rango/views.py
@@ -0,0 +1,7 @@
+from django.http.response import HttpResponseNotModified
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Rango says hey there partner!")
+
