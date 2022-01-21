8f890ee4fb306398d1295a647a70c4cd89cf1546
Ieuan Harries <2549037h@student.gla.ac.uk>
2022-01-21 15:10:04 +0000

    Completed Ch.4 & Tests

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,5 @@
 from xml.dom.minidom import Document
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from rango import views
 
 app_name = 'rango'
@@ -9,4 +7,4 @@ app_name = 'rango'
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
