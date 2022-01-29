f7db37d2cb93a9d03365ac002c4c7af9bd0a0f75
Ieuan Harries <2549037h@student.gla.ac.uk>
2022-01-29 17:06:50 +0000

    Completed Ch. 10 & Tests (AE Completed!)

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,6 @@
 from calendar import c
+from datetime import datetime
+import re
 from django.http.response import HttpResponseNotModified
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
@@ -18,7 +20,29 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
 
-    return render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    
+    request.session['visits'] = visits
+
 
 @login_required
 def add_page(request, category_name_slug):
@@ -82,7 +106,13 @@ def show_category(request, category_name_slug):
     return render(request, 'rango/category.html', context=context_dict)
 
 def about(request):
-    return render(request, 'rango/about.html')
+
+    visitor_cookie_handler(request)
+    context_dict = {}
+
+    context_dict['visits'] = request.session['visits']
+
+    return render(request, 'rango/about.html', context_dict)
 
 def register(request):
     registered = False
